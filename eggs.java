d03e87269791993107533fea3c4d9d0a00e01c07
Bradford Bonsack <bradford.bonsack@gmail.com>
2015-09-25 11:43:02 -0500

    Week 1 Novice Problem
    Almost complete need to add arrays.

---
+++ eggs.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class eggs {
+public static void main(String[] args) {
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("How many eggs do you have?");
+
+
+		//amount of egg
+		int n = input.nextInt();
+
+	
+		//max number of eggs
+		int p = input.nextInt();
+		if(p < 0 || p > 30){
+			System.out.println("");
+		}
+		// Weight of egg
+		int weight= input.nextInt();
+		if(weight < 0 || weight > 30){
+			System.out.println("");
+		}
+		//max weight of eggs
+		int q = input.nextInt();
+		if(q < 0 || q > 30){
+			System.out.println("");
+		}
+		while(weight<q && n<p){
+			
+		}
+	
+		int []
+	
+		
+	
+	
+}
+}
+}
